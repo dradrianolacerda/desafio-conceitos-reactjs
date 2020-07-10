4c254b689880ae5004d23c9a4685105c41a0fa3a
dradrianolacerda <67887669+dradrianolacerda@users.noreply.github.com>
2020-07-10 00:52:49 -0300

    Add files via upload

---
+++ build/precache-manifest.7e61b7ccb3ada835a88ace6aa5f76f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462ee6deed80789ed3c42420d44d4d9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73831408b478ef77cb75",
+    "url": "/static/css/main.0b137c96.chunk.css"
+  },
+  {
+    "revision": "42f002dc752540eda3e9",
+    "url": "/static/js/2.45b45fca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45b45fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73831408b478ef77cb75",
+    "url": "/static/js/main.233725df.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
